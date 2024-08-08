07637a1c3a9c3eeabf416478aa99041e8e2225ef
prince-konwea <konweaprince09@gmail.com>
2024-08-08 09:39:16 +0100

    feat: creating mongodb connection

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import *  as joi from 'joi';
 
-@Module({})
+@Module({
+    imports: [NestConfigModule.forRoot({
+        validationSchema: joi.object({
+            MONGODB_URI: joi.string().required(),
+        })
+    })]
+})
 export class ConfigModule {}
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+@Module({
+    imports: [MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+            uri: configService.get('MONGODB_URI'),
+            
+        }),
+      inject: [ConfigService]
+    })],
+})
 export class DatabaseModule {}
